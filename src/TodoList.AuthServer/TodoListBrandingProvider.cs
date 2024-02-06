a346fcc88de2f1a752a2044c6e4a6d0a84c70bdb
ayadi Med <ayadimed589@gmail.com>
2024-02-06 14:32:41 +0100

    first

---
+++ src/TodoList.AuthServer/TodoListBrandingProvider.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.Ui.Branding;
+using Volo.Abp.DependencyInjection;
+
+namespace TodoList;
+
+[Dependency(ReplaceServices = true)]
+public class TodoListBrandingProvider : DefaultBrandingProvider
+{
+    public override string AppName => "TodoList";
+}
---
+++ src/TodoList.DbMigrator/TodoListDbMigratorModule.cs
@@ -0,0 +1,21 @@
+﻿using TodoList.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Caching;
+using Volo.Abp.Caching.StackExchangeRedis;
+using Volo.Abp.Modularity;
+
+namespace TodoList.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(AbpCachingStackExchangeRedisModule),
+    typeof(TodoListEntityFrameworkCoreModule),
+    typeof(TodoListApplicationContractsModule)
+    )]
+public class TodoListDbMigratorModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpDistributedCacheOptions>(options => { options.KeyPrefix = "TodoList:"; });
+    }
+}
